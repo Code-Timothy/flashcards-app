50dc019d6f28a34cf715260a7389fa98898d19a7
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-12 20:37:23 +0100

    add theme file with ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { GlobalStyles } from "./core/globalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
 import Flashcards from "./features/Flashcards";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Flashcards />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/core/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+    colors: {
+        gunPowder: "#4A465B",
+    }
+};
