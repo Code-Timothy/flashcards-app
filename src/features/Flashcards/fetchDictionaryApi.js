998392d00f89e5f947d129dbd8a76d3013014742
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-12 23:10:39 +0100

    create fetchDictionaryApi file for API calls

---
+++ src/features/Flashcards/fetchDictionaryApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const fetchDictionaryApi = async () => {
+
+    const apiLink = `https://api.dictionaryapi.dev/api/v2/entries/en/hello`;
+
+    try {
+        const response = await axios.get(apiLink);
+
+        if (response && response.data) {
+            return response.data;
+        } else {
+            throw new Error("No data returned from API.");
+        }
+    } catch (error) {
+        console.error("Something went wrong!", error);
+    }
+};
