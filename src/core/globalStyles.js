7085a590d7b91ab039957fd4ab46586b3fe15761
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-12 20:34:12 +0100

    add alternative box-model

--- src/core/globalStyles.js
+++ src/core/globalStyles.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+    html{
+        box-sizing: border-box;
+    };
+
+    *, ::before, ::after{
+        box-sizing: inherit;
+    };
+
     body{
         font-family: "Nunito", serif;
         max-width: 1200px;
