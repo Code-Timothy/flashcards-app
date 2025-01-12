5ad59c11c94cd14ec9350bc430bac35f76e6ff38
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-12 23:07:44 +0100

    add reducers for fetching API to flashcardsSlice

--- src/features/Flashcards/flashcardsSlice.js
+++ src/features/Flashcards/flashcardsSlice.js
@@ -4,8 +4,24 @@ const flashcardsSlice = createSlice({
     name: "flashcards",
     initialState: {
         cards: [],
+        loading: false,
+        error: null,
     },
-    reducers: {},
+    reducers: {
+        fetchDictionaryApiRequest: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        fetchDictionaryApiSuccess: (state, action) => {
+            state.loading = false;
+            state.cards = action.payload;
+        },
+        fetchDictionaryApiFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    },
 });
 
+export const { fetchDictionaryApiRequest, fetchDictionaryApiSuccess, fetchDictionaryApiFailure } = flashcardsSlice.actions;
 export const flashcardsReducer = flashcardsSlice.reducer;
