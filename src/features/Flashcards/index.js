e0a89703e57f3dd35cd7f1a82e0174b09d949340
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-13 00:47:07 +0100

    create form for adding word and meaning with onFormSubmit handler

--- src/features/Flashcards/index.js
+++ src/features/Flashcards/index.js
@@ -1,11 +1,46 @@
-import { FlashCard, FlashCardContent, Wrapper } from "./styled";
+import { useState } from "react";
+import { Flashcard, FlashcardContent, Wrapper } from "./styled";
+import { useDispatch } from "react-redux";
+import { addFlashcard } from "./flashcardsSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Flashcards = () => {
+    const [word, setWord] = useState("");
+    const [meaning, setMeaning] = useState("");
+    const dispatch = useDispatch();
+    console.log(word, meaning)
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(addFlashcard({
+            id: nanoid(),
+            word,
+            meaning,
+        }));
+    };
+
     return (
         <Wrapper>
-                <FlashCard>
-                    <FlashCardContent></FlashCardContent>
-                </FlashCard>
+            <Flashcard>
+                <form onSubmit={onFormSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Enter a word"
+                        value={word}
+                        onChange={({ target }) => setWord(target.value)}
+                        required
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter a meaning"
+                        value={meaning}
+                        onChange={({ target }) => setMeaning(target.value)}
+                        required
+                    />
+                    <button>Dodaj</button>
+                </form>
+            </Flashcard>
         </Wrapper>
     );
 };
