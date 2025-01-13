f504b3ef09343df34d19b68e0566ca328399bdd5
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-13 01:34:40 +0100

    add input for category before adding flashcards

--- src/features/Flashcards/index.js
+++ src/features/Flashcards/index.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { Flashcard, FlashcardContent, Wrapper } from "./styled";
-import { useDispatch } from "react-redux";
-import { addFlashcard } from "./flashcardsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addCategory, addFlashcard, selectGlobalCategory } from "./flashcardsSlice";
 import { nanoid } from "@reduxjs/toolkit";
 
 const Flashcards = () => {
     const [word, setWord] = useState("");
     const [meaning, setMeaning] = useState("");
+    const [category, setCategory] = useState("");
+    const globalCategory = useSelector(selectGlobalCategory);
     const dispatch = useDispatch();
-    console.log(word, meaning)
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -17,31 +18,51 @@ const Flashcards = () => {
             id: nanoid(),
             word: word.trim(),
             meaning: meaning.trim(),
+            globalCategory: globalCategory || category,
         }));
         setWord("");
         setMeaning("");
     };
 
+    const onCategorySubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(addCategory(category.trim()));
+    };
+
     return (
         <Wrapper>
             <Flashcard>
-                <form onSubmit={onFormSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Enter a word"
-                        value={word}
-                        onChange={({ target }) => setWord(target.value)}
-                        required
-                    />
-                    <input
-                        type="text"
-                        placeholder="Enter a meaning"
-                        value={meaning}
-                        onChange={({ target }) => setMeaning(target.value)}
-                        required
-                    />
-                    <button>Dodaj</button>
-                </form>
+                {!globalCategory && (
+                    <form onSubmit={onCategorySubmit}>
+                        <input
+                            type="text"
+                            placeholder="Enter category"
+                            value={category}
+                            onChange={({ target }) => setCategory(target.value)}
+                        />
+                        <button>Add category</button>
+                    </form>
+                )}
+                {globalCategory && (
+                    <form onSubmit={onFormSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Enter a word"
+                            value={word}
+                            onChange={({ target }) => setWord(target.value)}
+                            required
+                        />
+                        <input
+                            type="text"
+                            placeholder="Enter a meaning"
+                            value={meaning}
+                            onChange={({ target }) => setMeaning(target.value)}
+                            required
+                        />
+                        <button>Add</button>
+                    </form>
+                )}
             </Flashcard>
         </Wrapper>
     );
