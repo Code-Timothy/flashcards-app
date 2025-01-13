dc6f47d4c09ceb9d9dffcbce1ead66a08c2719d4
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-13 16:34:44 +0100

    add 3 colors

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,7 +1,10 @@
 export const theme = {
     colors: {
         white: "#FFFFFF",
+        chatelle: "#ADA7C1",
         manatee: "#928CA6",
+        portage: "#9C81EA",
         gunPowder: "#4A465B",
+        steelGray: "#1F1D28",
     }
 };
