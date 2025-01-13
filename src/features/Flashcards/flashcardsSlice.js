fd0c33c158a7558d41c2b071c0bc0bd6427e7e98
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-13 18:46:10 +0100

    add action nextFlashcard

--- src/features/Flashcards/flashcardsSlice.js
+++ src/features/Flashcards/flashcardsSlice.js
@@ -7,6 +7,7 @@ const flashcardsSlice = createSlice({
         globalCategory: null,
         loading: false,
         error: null,
+        currentIndex: 0,
     },
     reducers: {
         addFlashcard: (state, action) => {
@@ -15,13 +16,23 @@ const flashcardsSlice = createSlice({
         addCategory: (state, action) => {
             state.globalCategory = action.payload;
         },
+        nextFlashcard: (state) => {
+            const nextIndex = state.currentIndex + 1;
+
+            if (nextIndex < state.flashcards.length) {
+                state.currentIndex = nextIndex;
+            } else {
+                state.currentIndex = 0;
+            }
+        },
     },
 });
 
-export const { addFlashcard, addCategory } = flashcardsSlice.actions;
+export const { addFlashcard, addCategory, nextFlashcard } = flashcardsSlice.actions;
 
 export const selectFlashcardsState = (state) => state.flashcards;
 export const selectFlashcards = (state) => selectFlashcardsState(state).flashcards;
 export const selectGlobalCategory = (state) => selectFlashcardsState(state).globalCategory;
+export const selectCurrentIndex = (state) => selectFlashcardsState(state).currentIndex;
 
 export const flashcardsReducer = flashcardsSlice.reducer;
--- src/features/Flashcards/pages/PractiseFlashcards/index.js
+++ src/features/Flashcards/pages/PractiseFlashcards/index.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import { Container } from "../../../../common/Container";
 import { Wrapper, Flashcard, FlashcardContent } from "../../styled";
-import { useSelector } from "react-redux";
-import { selectFlashcards } from "../../flashcardsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectFlashcards, nextFlashcard, selectCurrentIndex } from "../../flashcardsSlice";
 
 const PractiseFlashcards = () => {
     const [isFlipped, setIsFlipped] = useState(false);
     const flashcards = useSelector(selectFlashcards);
+    const currentIndex = useSelector(selectCurrentIndex);
+
+    const dispatch = useDispatch();
 
     const handleFlip = () => {
         setIsFlipped(flip => !flip);
     };
+
+    const currentFlashcard = flashcards[currentIndex] || {};
 
     return (
         <Container>
@@ -21,15 +26,17 @@ const PractiseFlashcards = () => {
                     <Wrapper $isFlipped={isFlipped} onClick={handleFlip}>
                         {!isFlipped && (
                             <Flashcard $isFlipped={isFlipped}>
-                                <FlashcardContent>{flashcards[0].word}</FlashcardContent>
+                                <FlashcardContent>{currentFlashcard.word}</FlashcardContent>
                             </Flashcard>
                         )}
                         {isFlipped && (
                             <Flashcard>
-                                <FlashcardContent $isFlipped={isFlipped}>{flashcards[0].meaning}</FlashcardContent>
+                                <FlashcardContent $isFlipped={isFlipped}>{currentFlashcard.meaning}</FlashcardContent>
                             </Flashcard>
                         )}
                     </Wrapper>
+                    <button onClick={() => dispatch(nextFlashcard())}>Next</button>
+                    <button onClick={handleFlip}>Flip the flishcard</button>
                 </>
             )}
         </Container>
