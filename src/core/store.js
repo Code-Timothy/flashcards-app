fd2d856102689f44de1e70f1badfb1711579aedc
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-15 02:42:29 +0100

    add middleware to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { flashcardsReducer } from "../features/Flashcards/flashcardsSlice";
+import { watchFlashcardsLocalStorage } from "../features/Flashcards/flashcardsSaga";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         flashcards: flashcardsReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(watchFlashcardsLocalStorage);
 
 export default store;
