e9d7989bab87384bccca1ecedcb93bccf8ec2b44
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-09 00:20:25 +0100

    change padding to 40px

--- src/features/Flashcards/pages/CreateFlashcards/CategoryTiles/styled.js
+++ src/features/Flashcards/pages/CreateFlashcards/CategoryTiles/styled.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
     flex-wrap: wrap;
     justify-content: center;
     gap: 20px;
-    padding: 20px;
+    padding: 40px;
 `;
 
 export const Tile = styled.button`
