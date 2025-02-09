38249bc7cb12ef42ce15457ad19a4c7c8a2cdb7f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-10 00:06:59 +0100

    add sagas fot fetchExampleFlashcardsHandler

--- src/features/Flashcards/flashcardsSaga.js
+++ src/features/Flashcards/flashcardsSaga.js
@@ -1,6 +1,7 @@
-import { call, takeEvery, select } from "redux-saga/effects";
+import { call, takeEvery, select, put, takeLatest } from "redux-saga/effects";
 import { saveFlashcardsInLocalStorage } from "./flashcardsLocalStorage";
-import { selectFlashcardsState } from "./flashcardsSlice";
+import { fetchExampleFlashcardsRequest, fetchExampleFlashcardsSuccess, selectFlashcardsState } from "./flashcardsSlice";
+import { fetchExampleFlashcards } from "./fetchExampleFlashcards";
 
 function* flashcardsLocalStorageHandler() {
     try {
@@ -11,6 +12,19 @@ function* flashcardsLocalStorageHandler() {
     }
 };
 
+function* fetchExampleFlashcardsHandler() {
+    try {
+        const exampleFlashcards = yield (call(fetchExampleFlashcards));
+        yield put(fetchExampleFlashcardsSuccess(exampleFlashcards));
+    } catch (error) {
+        yield call(alert, "Something went wrong with fetching example flashcards.");
+    }
+};
+
 export function* watchFlashcardsLocalStorage() {
     yield takeEvery("*", flashcardsLocalStorageHandler);
+};
+
+export function* watchFetchExampleFlashcards() {
+    yield takeLatest(fetchExampleFlashcardsRequest.type, fetchExampleFlashcardsHandler);
 };
